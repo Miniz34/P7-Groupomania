d1795c29815fc8e2138e8906b541d8e2ac3fba73
Karim Farhi <ka.farhi@gmail.com>
2022-05-09 15:34:29 +0200

    premier appel d'api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "axios"
 
 import './assets/styles/bootstrap.min.css'
 import './assets/styles/style.css'
--- frontend/src/views/PublicationsView.vue
+++ frontend/src/views/PublicationsView.vue
@@ -3,17 +3,17 @@
   <head>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" />
   </head>
-  <div class="publication" id="publication">
-    <div v-for="post in publication" v-bind:key="post" class="container">
-      <div class="well">
+  <div class="Publications-View" id="Publications-View">
+    <div class="container">
+      <div v-for="publication in publications" :key="publication" class="well ">
         <div class="media">
           <a class="pull-left" href="#">
             <img class="media-object" src="http://placekitten.com/150/150">
           </a>
           <div class="media-body">
-            <h4 class="media-heading">{{ post }}</h4>
-            <p class="text-right">By Francisco</p>
-            <p>sup</p>
+            <h4 class="media-heading">{{ publication.id }}</h4>
+            <p class="text-right">by userid : {{ publication.userId }}</p>
+            <p>{{ publication.content }}</p>
             <ul class="list-inline list-unstyled main-post">
               <li><span><i class="glyphicon glyphicon-calendar"></i> 2 days, 8 hours </span></li>
               <li>|</li>
@@ -37,43 +37,51 @@
           </div>
         </div>
       </div>
+      <div>
+        <button v-on:click="mounted"> Appel d'api parce que je sais pas faire autrement</button>
+      </div>
 
     </div>
   </div>
 
-  <p>{{ publication }}</p>
+  <!-- <p>{{ publication }}</p>
+  <p>{{ user }}</p>
+  <p>{{ publication }}</p> -->
+
+  <div v-for="publication in publications" :key="publication" class="post">
+    {{ publication }}
+  </div>
 </template>
 
 <script>
 
+import axios from 'axios';
 
 export default {
-  name: "publication",
+  name: "Publications-View",
   data() {
     return {
-      publication: {
-        id: "8",
-        content: "azezaee",
-        userId: "14"
-      },
-
+      publications: []
 
     };
   },
   methods: {
-    async mounted() {
-      const res = await fetch('http://localhost:3000/api/publication/get')
-      const data = await res.json()
-      this.publication = data
-      console.log(data)
+
+    mounted() {
+      // GET request using axios with error handling
+      axios.get("http://localhost:3000/api/publication/get")
+        .then(function (response) {
+          this.publications = response.data
+        }.bind(this));
+
     },
     testClick() {
       const test = console.log("bonjours")
       return test
-    }
+    },
   }
-
 }
+
 
 
 
@@ -89,4 +97,33 @@ export default {
 .main-post {
   display: flex;
 }
+
+.post {
+  width: 300px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  background: #FFEEE4;
+  margin: 10px;
+}
 </style>
+
+
+ //   async mounted() {
+    //     const res = await fetch('http://localhost:3000/api/publication/get')
+    //     const data = await res.json()
+    //     this.publication = data
+    //   },
+    //   testClick() {
+    //     const test = console.log("bonjours")
+    //     return test
+    //   },
+
+    //   loadUsers() {
+    //     axios.get('http://localhost:3000/api/publication/get')
+    //       .then(function (response) {
+    //         this.publication = response.data;
+    //       }.bind(this));
+    //   }
+    // }
