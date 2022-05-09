69ab6e9ac6f7ea34d5be1a948728e56e5c65c3c0
Karim Farhi <ka.farhi@gmail.com>
2022-05-09 20:01:58 +0200

    ajout erreur et input signup

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
   <form id="Auth-View">
     <section class="d-flex flex-column main-form w-50 align-center main-form">
+      <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
       <input v-model="dataLogin.username" class=" form-control form-control-lg" type="text" placeholder="Username"
-        aria-label=".form-control-lg example">
+        aria-label=".form-control-lg example" id="username">
+      <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
       <input v-model="dataLogin.password" class=" form-control form-control-lg" type="text" placeholder="Password"
-        aria-label=".form-control-lg example">
+        aria-label=".form-control-lg example" id="password">
+      <!-- <div class="valid-feedback">
+        Looks good!
+      </div> -->
+
       <button @click.prevent="mounted" type="submit" class="btn btn-primary">Submit</button>
     </section>
   </form>
 
-  <button v-on:click="mounted">random button</button>
 </template>
 
 
@@ -34,20 +39,25 @@ export default {
 
   methods: {
     mounted() {
-      // GET request using axios with error handling
-      fetch("http://localhost:3000/api/user/auth", {
-        method: "POST",
-        body: JSON.stringify({
-          username: this.dataLogin.username,
-          password: this.dataLogin.password
-        }),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-        .then((response) => response.json())
 
+
+      if (this.dataLogin.username && this.dataLogin.password) {
+        fetch("http://localhost:3000/api/user/auth", {
+          method: "POST",
+          body: JSON.stringify({
+            username: this.dataLogin.username,
+            password: this.dataLogin.password
+          }),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then((response) => response.json())
+
+      } else {
+        alert("Veuillez remplir le formulaire d'inscription")
+      }
     }
   }
 }
