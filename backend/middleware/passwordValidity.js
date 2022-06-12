4f8780a3ed13994d520f7b246f2548a657cc6ef4
Karim Farhi <ka.farhi@gmail.com>
2022-06-12 10:27:11 +0200

    update pw validator

--- backend/middleware/passwordValidity.js
+++ backend/middleware/passwordValidity.js
@@ -4,7 +4,7 @@ const passwordSchema = require('../models/password');
 
 module.exports = (req, res, next) => {
   if (!passwordSchema.validate(req.body.password)) { //Si le mot de passe n'est pas valide
-    res.writeHead(400, '{"au moins 3 caractères : 1 majuscule, 1 minuscule, 1 chiffre HELLO IS THIS WORKING"}', {
+    res.writeHead(400, '{"au moins 3 caractères : 1 majuscule, 1 minuscule, 1 chiffre"}', {
       'content-type': 'application/json'
     });
     res.end('Format de mot de passe incorrect');
--- backend/middleware/passwordValidityModify.js
+++ backend/middleware/passwordValidityModify.js
@@ -4,7 +4,7 @@ const passwordSchema = require('../models/password');
 
 module.exports = (req, res, next) => {
   if (!passwordSchema.validate(req.body.repeatNewPwd)) { //Si le mot de passe n'est pas valide
-    res.writeHead(400, '{"au moins 3 caractères : 1 majuscule, 1 minuscule, 1 chiffre GTGTGTGTGTGTGTGT"}', {
+    res.writeHead(400, '{"au moins 3 caractères : 1 majuscule, 1 minuscule, 1 chiffre"}', {
       'content-type': 'application/json'
     });
     res.end('Format de mot de passe incorrect');
